c3637ee3703afe8e5e126d3917289b75cdac48d0
Gyan Aryan <gyanaryan1519@gmail.com>
2026-07-27 09:33:57 +0530

    Pagination storybook (#43)

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
-import { components } from "./componentsList";
 import "./index.css";
-import { styles } from "./styles";
-
 const App = () => {
     return (
         <div style={{ padding: "20px" }}>
-            <h2>Component Library</h2>
-
-            {components.map((comp, index) => (
-                <div key={index} style={styles.componentsContainer}>
-                    <h4 style={{ marginBottom: "10px" }}>{comp.name}</h4>
-                    {comp.render()}
-                </div>
-            ))}
+            <h2>For components checkout : npm run storybook</h2>
         </div>
     );
 };
